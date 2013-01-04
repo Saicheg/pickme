273e0c400f0acf433755cc7a3221156cfdf52f70
Valentine Zavadsky <saicheg@gmail.com>
2013-01-04 21:06:22 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pickme::Application.config.secret_token = '003552e7ec8978537e279438c2fa936afd0c1109770a9fd171c693f11c9b95623d1920047123273de54d60f2c2b089f5ae70277d844675b55ec7a1930af66c86'
