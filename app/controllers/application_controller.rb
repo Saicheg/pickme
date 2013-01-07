05cbfe6fdf4995316d84f3a7aa24fcd151e779ed
Yury Kaliada <fut.wrk@gmail.com>
2013-01-07 12:47:26 +0300

    Authentication key validation
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :validate_auth_key
+
+  def validate_auth_key
+    raise ActionController::RoutingError.new I18n.t('error.auth_key') unless auth_key_valid?
+  end
+
+  private
+  def auth_key_valid?
+    key_string = "#{params[:api_id]}_#{params[:viewer_id]}_#{Settings[:api_secret]}"
+    params[:auth_key] == Digest::MD5.hexdigest(key_string)
+  end
 end
