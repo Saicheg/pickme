400f8d6ff6c7992e254fd3b607f6fb7962983765
Yury Kaliada <fut.wrk@gmail.com>
2013-01-14 22:04:52 +0300

    Minor syntax fix
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -2,9 +2,9 @@ class Pick
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  belongs_to :winner, class_name: "User", inverse_of: :picks
-  belongs_to :loser,  class_name: "User", inverse_of: :loses
-  belongs_to :player, class_name: "User", inverse_of: :plays
+  belongs_to :winner, class_name: 'User', inverse_of: :picks
+  belongs_to :loser,  class_name: 'User', inverse_of: :loses
+  belongs_to :player, class_name: 'User', inverse_of: :plays
 
   after_create :update_ratings
 
